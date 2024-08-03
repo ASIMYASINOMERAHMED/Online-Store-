a994aab5839ffa8024829a366158953bf80839e7
Asim Yasin <104110152+ASIMYASINOMERAHMED@users.noreply.github.com>
2024-08-03 15:25:06 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Online_Store_Project
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+    
+        static void Main()
+        {
+            if (Environment.OSVersion.Version.Major >= 6)
+                SetProcessDPIAware();
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new MultiFormContext(new LoginScreenBar(),new LoginScreenBar()));
+            Application.Run(new frmLogin());
+
+        }
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+
+        private static extern bool SetProcessDPIAware();
+    }
+}
